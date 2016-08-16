stalniy/ng2-cordova-oauth

refactor(provider): moves login method to cordovaOauth and change its name to logInVia

oauth.d.ts
@@ -1,7 +1,8 @@
+import { Provider } from './provider';
 export declare class CordovaOauth {
-    _provider: IOauthProvider;
-    constructor(provider: IOauthProvider);
-    login(config: any, browserOptions: any): Promise<{}>;
+    constructor();
+    logInVia(provider: Provider, browserOptions?: Object): Promise<{}>;
+    protected serializeBrowserOptions(options: Object): string;
 }
 export interface IOauthProvider {
     login(options?: Object): Promise<Object>;

src/oauth.ts
@@ -4,37 +4,68 @@
  * http://www.nraboy.com
  */
 
+import { Provider } from './provider';
+
 declare var window: any;
+
+const DEFAULT_BROWSER_OPTIONS = {
+  location: 'no',
+  clearsessioncache: 'yes',
+  clearcache: 'yes'
+};
 
 /*
  * The main driver class for connections to each of the providers.
  */
 export class CordovaOauth {
 
-    _provider: IOauthProvider;
+    constructor() {
+      if (!window.cordova) {
+          throw new Error('Cannot authenticate via a web browser');
+      }
 
-    constructor(provider: IOauthProvider) {
-        this._provider = provider;
+      if (!window.cordova.InAppBrowser) {
+          throw new Error('The Apache Cordova InAppBrowser plugin was not found and is required');
+      }
     }
 
-    login(config, browserOptions) {
+    logInVia(provider: Provider, browserOptions: Object = {}) {
+        const url = provider.dialogUrl();
+        const windowParams = this.serializeBrowserOptions(
+          utils.defaults(browserOptions, DEFAULT_BROWSER_OPTIONS)
+        );
+
         return new Promise((resolve, reject) => {
-            if (window.cordova) {
-                if (window.cordova.InAppBrowser) {
-                    this._provider.login(config, browserOptions).then((success) => {
-                        resolve(success);
-                    }, (error) => {
-                        reject(error);
-                    });
-                } else {
-                    reject("The Apache Cordova InAppBrowser plugin was not found and is required");
+            const browserRef = window.cordova.InAppBrowser.open(url, '_blank', windowParams);
+            const exitListener = () => reject(`The "${provider.name}" sign in flow was canceled`);
+
+            browserRef.addEventListener('loadstart', (event) => {
+                if (event.url.indexOf(provider.options.redirectUri) === 0) {
+                    browserRef.removeEventListener('exit', exitListener);
+                    browserRef.close();
+
+                    try {
+                      resolve(provider.parseResponseInUrl(event.url));
+                    } catch (e) {
+                      reject(e);
+                    }
                 }
-            } else {
-                reject("Cannot authenticate via a web browser");
-            }
-        });
+            })
+            browserRef.addEventListener('exit', exitListener);
+        })
     }
 
+    protected serializeBrowserOptions(options: Object) {
+      const chunks = [];
+
+      for (const prop in options) {
+        if (options.hasOwnProperty(prop)) {
+          chunks.push(`${prop}=${options[prop]}`);
+        }
+      }
+
+      return chunks.join(',');
+    }
 }
 
 /*

nraboy/ngx-cordova-oauth

oauth.d.ts
@@ -1,8 +1,9 @@
+import { OAuthProvider } from './provider';
 export declare class CordovaOauth {
-    _provider: IOauthProvider;
-    constructor(provider: IOauthProvider);
-    login(config: any, browserOptions: any): Promise<{}>;
+    login(provider: OAuthProvider, browserOptions?: Object): Promise<{}>;
+    logInVia(provider: OAuthProvider, browserOptions?: Object): Promise<{}>;
+    protected serializeBrowserOptions(options: Object): string;
 }
 export interface IOauthProvider {
-    login(options: Object, browserOptions?: Object): Promise<Object>;
+    parseResponseInUrl(url: string): Object;
 }

src/oauth.ts
@@ -4,37 +4,75 @@
  * http://www.nraboy.com
  */
 
+import { OAuthProvider } from './provider';
+import { utils } from './utility';
+
 declare var window: any;
+
+const DEFAULT_BROWSER_OPTIONS = {
+  location: 'no',
+  clearsessioncache: 'yes',
+  clearcache: 'yes'
+};
 
 /*
  * The main driver class for connections to each of the providers.
  */
 export class CordovaOauth {
 
-    _provider: IOauthProvider;
-
-    constructor(provider: IOauthProvider) {
-        this._provider = provider;
+    login(provider: OAuthProvider, browserOptions: Object = {}) {
+      console.warn(`
+        new CordovaOauth().login(...) is deprecated and will be removed in the next release.
+        Please use new CordovaOauth().logInVia(...) instead.
+      `)
+      return this.logInVia(provider, browserOptions)
     }
 
-    login(config, browserOptions) {
+    logInVia(provider: OAuthProvider, browserOptions: Object = {}) {
+        const url = provider.dialogUrl();
+        const windowParams = this.serializeBrowserOptions(
+          utils.defaults(browserOptions, DEFAULT_BROWSER_OPTIONS)
+        );
+
         return new Promise((resolve, reject) => {
-            if (window.cordova) {
-                if (window.cordova.InAppBrowser) {
-                    this._provider.login(config, browserOptions).then((success) => {
-                        resolve(success);
-                    }, (error) => {
-                        reject(error);
-                    });
-                } else {
-                    reject("The Apache Cordova InAppBrowser plugin was not found and is required");
-                }
-            } else {
-                reject("Cannot authenticate via a web browser");
+            if (!window.cordova) {
+                return reject(new Error('Cannot authenticate via a web browser'));
             }
-        });
+
+            if (!window.cordova.InAppBrowser) {
+                return reject(new Error('The Apache Cordova InAppBrowser plugin was not found and is required'));
+            }
+
+            const browserRef = window.cordova.InAppBrowser.open(url, '_blank', windowParams);
+            const exitListener = () => reject(`The "${provider.name}" sign in flow was canceled`);
+
+            browserRef.addEventListener('loadstart', (event) => {
+                if (event.url.indexOf(provider.options.redirectUri) === 0) {
+                    browserRef.removeEventListener('exit', exitListener);
+                    browserRef.close();
+
+                    try {
+                      resolve(provider.parseResponseInUrl(event.url));
+                    } catch (e) {
+                      reject(e);
+                    }
+                }
+            })
+            return browserRef.addEventListener('exit', exitListener);
+        })
     }
 
+    protected serializeBrowserOptions(options: Object) {
+      const chunks = [];
+
+      for (const prop in options) {
+        if (options.hasOwnProperty(prop)) {
+          chunks.push(`${prop}=${options[prop]}`);
+        }
+      }
+
+      return chunks.join(',');
+    }
 }
 
 /*
@@ -42,5 +80,5 @@ export class CordovaOauth {
  * consistency between the providers.
  */
 export interface IOauthProvider {
-    login(options: Object, browserOptions?: Object): Promise<Object>;
+    parseResponseInUrl(url: string): Object;
 }